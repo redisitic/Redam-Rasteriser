237b1fb4332737f4ab55d1df90a1fa7901771223
Redisitic <rhythmjuneja7845@gmail.com>
2025-06-01 14:05:10 +0530

    Added Depth Buffer

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@
         public float r { get => x; set => x = value; }
         public float g { get => y; set => y = value; }
         public float b { get => z; set => z = value; }
+        public static float Dot(float3 a, float3 b) => a.x * b.x + a.y * b.y + a.z * b.z;
         public static float3 operator +(float3 a, float3 b) => new float3(a.x + b.x, a.y + b.y, a.z + b.z);
         public static float3 operator -(float3 a, float3 b) => new float3(a.x - b.x, a.y - b.y, a.z - b.z);
         public static float3 operator *(float3 a, float scalar) => new float3(a.x * scalar, a.y * scalar, a.z * scalar);
@@ -31,15 +32,15 @@
             this.x = x;
             this.y = y;
         }
+        public static implicit operator float2(float3 v) => new float2(v.x, v.y);
         public static float2 operator +(float2 a, float2 b) => new float2(a.x + b.x, a.y + b.y);
         public static float2 operator -(float2 a, float2 b) => new float2(a.x - b.x, a.y - b.y);
         public static float2 operator *(float2 a, float scalar) => new float2(a.x * scalar, a.y * scalar);
-        public static float2 operator *(float2 a, float2 b) => new float2(a.x*b.x, a.y*b.y);
+        public static float2 operator *(float2 a, float2 b) => new float2(a.x * b.x, a.y * b.y);
         public static float2 operator *(float scalar, float2 a) => a * scalar;
         public static float2 operator /(float2 a, float scalar) => new float2(a.x / scalar, a.y / scalar);
         public override string ToString() => $"({x}, {y})";
     }
-
     public static float Dot(float2 a, float2 b) => a.x * b.x + a.y * b.y;
     public static float2 Perpendicular(float2 v) => new float2(-v.y, v.x);
     public static bool PointOnRight(float2 a, float2 b, float2 p)
@@ -49,20 +50,12 @@
         float cross = ab.x * ap.y - ab.y * ap.x;
         return cross < 0;
     }
-    static bool PointInTriangle(float2 a, float2 b, float2 c, float2 p)
-    {
-        bool sideAB = PointOnRight(a, b, p);
-        bool sideBC = PointOnRight(b, c, p);
-        bool sideCA = PointOnRight(c, a, p);
-        return (sideAB && sideBC && sideCA);
-    }
     public static float2 RandomFloat2(Random rng, float width, float height)
     {
         float x = (float)rng.NextDouble() * width;
         float y = (float)rng.NextDouble() * height;
         return new float2(x, y);
     }
-
     static void WriteImagetoFile(float3[,] image, string fileName)
     {
         using (BinaryWriter writer = new BinaryWriter(File.Open(fileName + ".bmp", FileMode.Create)))
@@ -103,57 +96,6 @@
             }
         }
     }
-    public class RenderTarget(int w, int h)
-    {
-        public readonly float3[,] ColourBuffer = new float3[w, h];
-        public readonly int Width = w;
-        public readonly int Height = h;
-        public readonly float2 Size = new float2(w, h);
-    }
-    public class Model(float3[] points, float3[] cols)
-    {
-        public readonly float3[] OriginalPoints = points;
-        public readonly float3[] TriangleCols = cols;
-        public Transform Transform = new Transform();
-        public float3[] GetTransformedPoints()
-        {
-            float3[] transformed = new float3[OriginalPoints.Length];
-            for (int i = 0; i < OriginalPoints.Length; i++)
-            {
-                transformed[i] = Transform.ToWorldPoint(OriginalPoints[i]);
-            }
-            return transformed;
-        }
-    }
-
-    public class Transform
-    {
-        public float yaw;
-        public float pitch;
-        public float3 Position;
-        public float3 ToWorldPoint(float3 p)
-        {
-            (float3 ihat, float3 jhat, float3 khat) = GetBasisVectors();
-            return TransformVector(ihat, jhat, khat, p) + Position;
-        }
-        private (float3 ihat, float3 jhat, float3 khat) GetBasisVectors()
-        {
-            float3 ihat_yaw = new float3((float)Math.Cos(yaw), 0, (float)Math.Sin(yaw));
-            float3 jhat_yaw = new float3(0, 1, 0);
-            float3 khat_yaw = new float3(-(float)Math.Sin(yaw), 0, (float)Math.Cos(yaw));
-            float3 ihat_pitch = new float3(1, 0, 0);
-            float3 jhat_pitch = new float3(0, (float)Math.Cos(pitch), -(float)Math.Sin(pitch));
-            float3 khat_pitch = new float3(0, (float)Math.Sin(pitch), (float)Math.Cos(pitch));
-            float3 ihat = TransformVector(ihat_yaw, jhat_yaw, khat_yaw, ihat_pitch);
-            float3 jhat = TransformVector(ihat_yaw, jhat_yaw, khat_yaw, jhat_pitch);
-            float3 khat = TransformVector(ihat_yaw, jhat_yaw, khat_yaw, khat_pitch);
-            return (ihat, jhat, khat);
-        }
-        private float3 TransformVector(float3 ihat, float3 jhat, float3 khat, float3 v)
-        {
-            return (v.x * ihat) + (v.y * jhat) + (v.z * khat);
-        }
-    }
     static IEnumerable<string> SplitByLine(string input)
     {
         return input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
@@ -185,27 +127,119 @@
         }
         return trianglePoints.ToArray();
     }
-    static float2 VertexToScreen(float3 vertex_world, float2 screenSize, float fov)
+    public class RenderTarget(int w, int h)
     {
-        float screenHeight_world = (float)Math.Tan(fov / 2) * 2; // Explicit cast added to fix CS0266
+        public readonly float3[,] ColourBuffer = new float3[w, h];
+        public readonly float[,] DepthBuffer = new float[w, h];
+        public readonly int Width = w;
+        public readonly int Height = h;
+        public readonly float2 Size = new float2(w, h);
+    }
+    public class Model(float3[] points, float3[] cols)
+    {
+        public readonly float3[] OriginalPoints = points;
+        public readonly float3[] TriangleCols = cols;
+        public Transform Transform = new Transform();
+        public float3[] GetTransformedPoints()
+        {
+            float3[] transformed = new float3[OriginalPoints.Length];
+            for (int i = 0; i < OriginalPoints.Length; i++)
+            {
+                transformed[i] = Transform.ToWorldPoint(OriginalPoints[i]);
+            }
+            return transformed;
+        }
+    }
+    public class Transform
+    {
+        public float yaw;
+        public float pitch;
+        public float3 Position;
+        public float3 ToWorldPoint(float3 p)
+        {
+            (float3 ihat, float3 jhat, float3 khat) = GetBasisVectors();
+            return TransformVector(ihat, jhat, khat, p) + Position;
+        }
+        private (float3 ihat, float3 jhat, float3 khat) GetBasisVectors()
+        {
+            float3 ihat_yaw = new float3((float)Math.Cos(yaw), 0, (float)Math.Sin(yaw));
+            float3 jhat_yaw = new float3(0, 1, 0);
+            float3 khat_yaw = new float3(-(float)Math.Sin(yaw), 0, (float)Math.Cos(yaw));
+            float3 ihat_pitch = new float3(1, 0, 0);
+            float3 jhat_pitch = new float3(0, (float)Math.Cos(pitch), -(float)Math.Sin(pitch));
+            float3 khat_pitch = new float3(0, (float)Math.Sin(pitch), (float)Math.Cos(pitch));
+            float3 ihat = TransformVector(ihat_yaw, jhat_yaw, khat_yaw, ihat_pitch);
+            float3 jhat = TransformVector(ihat_yaw, jhat_yaw, khat_yaw, jhat_pitch);
+            float3 khat = TransformVector(ihat_yaw, jhat_yaw, khat_yaw, khat_pitch);
+            return (ihat, jhat, khat);
+        }
+        private float3 TransformVector(float3 ihat, float3 jhat, float3 khat, float3 v)
+        {
+            return (v.x * ihat) + (v.y * jhat) + (v.z * khat);
+        }
+    }
+    public static float SignedTriangleArea(float2 a, float2 b, float2 c)
+    {
+        float2 ac = c - a;
+        float2 abPerp = Perpendicular(b - a);
+        return Dot(ac, abPerp) / 2f;
+    }
+    static bool PointInTriangle(float2 a, float2 b, float2 c, float2 p, out float3 weights)
+    {
+        float areaABP = SignedTriangleArea(a, b, p);
+        float areaBCP = SignedTriangleArea(b, c, p);
+        float areaCAP = SignedTriangleArea(c, a, p);
+        bool inTri = areaABP <= 0 && areaBCP <= 0 && areaCAP <= 0;
+        float totalArea = Math.Abs(areaABP + areaBCP + areaCAP);
+        if (totalArea == 0)
+        {
+            weights = new float3(0, 0, 0);
+            return false;
+        }
+        float invArea = 1f / totalArea;
+        float weightA = Math.Abs(areaBCP) * invArea;
+        float weightB = Math.Abs(areaCAP) * invArea;
+        float weightC = Math.Abs(areaABP) * invArea;
+        weights = new float3(weightA, weightB, weightC);
+        return inTri && totalArea > 0;
+    }
+    static float3 VertexToScreen(float3 vertex_world, float2 screenSize, float fov)
+    {
+        float screenHeight_world = (float)Math.Tan(fov / 2) * 2;
         float pixelsPerWorldUnit = screenSize.y / screenHeight_world / vertex_world.z;
 
         float2 pixelOffset = new float2(vertex_world.x, vertex_world.y) * pixelsPerWorldUnit;
-        return screenSize / 2 + pixelOffset;
+        float2 vertexScreen = screenSize / 2 + pixelOffset;
+        return new float3(vertexScreen.x, vertexScreen.y, vertex_world.z);
     }
-    static float3[,] Render(Model model, RenderTarget target, int frame)
+    static void ClearBuffers(RenderTarget target)
+    {
+        int width = target.Width;
+        int height = target.Height;
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                target.ColourBuffer[x, y] = new float3(0, 0, 0);
+                target.DepthBuffer[x, y] = float.MaxValue;
+            }
+        }
+    }
+    static float3[,] Render(Model model, RenderTarget target, int frame, bool visualizeDepth = false)
     {
         int width = target.Width;
         int height = target.Height;
         float fov = 60f * (float)Math.PI / 180f;
         float3[,] image = target.ColourBuffer;
         float3[] points = model.GetTransformedPoints();
-        float3[] triangleCols = model.TriangleCols; 
+        float3[] triangleCols = model.TriangleCols;
+        float nearPlane = 1f;
+        float farPlane = 5f;
         for (int i = 0; i < points.Length; i += 3)
         {
-            float2 a = VertexToScreen(points[i], target.Size, fov);
-            float2 b = VertexToScreen(points[i + 1], target.Size, fov);
-            float2 c = VertexToScreen(points[i + 2], target.Size, fov);  
+            float3 a = VertexToScreen(points[i], target.Size, fov);
+            float3 b = VertexToScreen(points[i + 1], target.Size, fov);
+            float3 c = VertexToScreen(points[i + 2], target.Size, fov);
             float minXf = MathF.Min(a.x, MathF.Min(b.x, c.x));
             float maxXf = MathF.Max(a.x, MathF.Max(b.x, c.x));
             float minYf = MathF.Min(a.y, MathF.Min(b.y, c.y));
@@ -220,55 +254,54 @@
                 for (int x = minX; x <= maxX; x++)
                 {
                     float2 p = new float2(x, y);
-                    if (PointInTriangle(a, b, c, p))
+                    float3 weights;
+                    if (PointInTriangle((float2)a, (float2)b, (float2)c, p, out weights))
                     {
-                        image[x, y] = col;
+                        float3 depths = new float3(a.z, b.z, c.z);
+                        float depth = float3.Dot(depths, weights);
+                        if (depth < target.DepthBuffer[x, y])
+                        {
+                            if (visualizeDepth)
+                            {
+                                float normalizedDepth = MathF.Max(0, MathF.Min(1, (depth - nearPlane) / (farPlane - nearPlane)));
+                                image[x, y] = new float3(normalizedDepth, normalizedDepth, normalizedDepth);
+                            }
+                            else
+                            {
+                                image[x, y] = col;
+                            }
+                            target.DepthBuffer[x, y] = depth;
+                        }
                     }
                 }
             }
         }
         return image;
     }
-    static float3[] ToFloat3(float2[] points)
-    {
-        float3[] result = new float3[points.Length];
-        for (int i = 0; i < points.Length; i++)
-        {
-            result[i] = new float3(points[i].x, points[i].y, 0);
-        }
-        return result;
-    }
-    static void ClearImage(float3[,] image)
-    {
-        int width = image.GetLength(0);
-        int height = image.GetLength(1);
-        for (int y = 0; y < height; y++)
-            for (int x = 0; x < width; x++)
-                image[x, y] = new float3(0, 0, 0);
-    }
     public static void RenderModel()
     {
-        string objPath = Path.Combine(Directory.GetCurrentDirectory(), "models", "cube.obj");
+        string objPath = Path.Combine(Directory.GetCurrentDirectory(), "models", "suzanne.obj");
         string objString = File.ReadAllText(objPath);
         float3[] cubeModelPoints = LoadOBJFile(objString);
         Random rng = new();
         float3[] triangleCols = new float3[cubeModelPoints.Length / 3];
         for (int i = 0; i < triangleCols.Length; i++)
         {
-            triangleCols[i] = new float3(rng.Next(256) / 255f, rng.Next(256) / 255f, rng.Next(256) / 255f);
+            triangleCols[i] = new float3(((rng.Next(128) + 112) / 255f), ((rng.Next(128) + 112) / 255f), ((rng.Next(128) + 112) / 255f));
         }
         Model cubeModel = new Model(cubeModelPoints, triangleCols);
-        cubeModel.Transform.Position = new float3(0, 0, 4f);
+        cubeModel.Transform.Position = new float3(0, 0, 3f);
+        float inYaw = (float)Math.PI;
         RenderTarget renderTarget = new RenderTarget(960, 540);
-        float3[,] image = new float3[renderTarget.Width, renderTarget.Height];
+        bool renderDepth = false;
         const int frameCount = 120;
         for (int frame = 0; frame < frameCount; frame++)
         {
-            cubeModel.Transform.yaw = (float)(frame * Math.PI * 2f / frameCount);
-            cubeModel.Transform.pitch = (float)(frame * Math.PI * 2f / frameCount);
-            ClearImage(renderTarget.ColourBuffer);
-            WriteImagetoFile(Render(cubeModel, renderTarget, frame), $"cube_frame_{frame:D2}");
-            Console.WriteLine($"Cube image created for frame {frame}");
+            cubeModel.Transform.yaw = (float)(frame * Math.PI * 2f / frameCount) + inYaw;
+            ClearBuffers(renderTarget);
+            Render(cubeModel, renderTarget, frame, renderDepth);
+            WriteImagetoFile(renderTarget.ColourBuffer, $"Raster{frame:D2}");
+            Console.WriteLine($"Raster image created for frame {frame}");
         }
     }
     public static void Main(string[] args)
--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Redam-Rasteriser
 A software rasterizer based on one made by Sebastian Lague (https://www.youtube.com/watch?v=yyJ-hdISgnw&t)
-![alt text](https://github.com/redisitic/Redam-Rasteriser/blob/master/gitRender.gif)
+## Colour Output
+![alt text](https://github.com/redisitic/Redam-Rasteriser/blob/master/gitImages/gitRender.gif)
+## Depth Output
+![alt text](https://github.com/redisitic/Redam-Rasteriser/blob/master/gitImages/gitDepth.gif)
+
+## Current Features:
+- Perspective Projection
+- Backface Culling
+- Depth Buffering
+- Animation Output
+
+## ToDo Features:
+- Texture Mapping
+- Phong Shading
+- Normal Mapping
+- Render in Game Window
